e11f62605d161da979b5ed28ef1908ceb6abb3f5
kiranautomate <kiran.automate@gmail.com>
2015-09-13 15:42:22 +0530

    Changed password

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'debras');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '1l0vep@1n');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
